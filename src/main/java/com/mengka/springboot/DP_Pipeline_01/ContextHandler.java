55a8d98f2e5430413ec9541b68223bde344c02c8
934734735@qq.com <934734735@qq.com>
2020-12-03 11:09:35 +0800

    管道模式（Pipeline Pattern）

--- src/main/java/com/mengka/springboot/DP_Pipeline_01/ContextHandler.java
+++ src/main/java/com/mengka/springboot/DP_Pipeline_01/ContextHandler.java
@@ -5,6 +5,8 @@ import com.mengka.springboot.DP_Pipeline_01.context.PipelineContext;
 /**
  * 管道中的上下文处理器
  *
+ * 文档：
+ * https://mp.weixin.qq.com/s?__biz=MzAxNDEwNjk5OQ==&mid=2650413430&idx=1&sn=32c89ea3222d341bf3014854f69fd239&chksm=8396d16eb4e1587838f99a6859668a3588b0762155c1a13872d3f419efa3f75c5a532d7d38fe&scene=21#wechat_redirect
  */
 public interface ContextHandler<T extends PipelineContext> {
 
